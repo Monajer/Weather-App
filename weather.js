2450469b06e793b88d27a21b298d55148e2cdf4e
Monajer <monajeralhakim@gmail.com>
2023-11-11 00:49:47 +0600

    weather

---
+++ weather.js
@@ -0,0 +1,12 @@
+// const data = document.getElementById ("data")
+
+// const apiKye = "8526b09fa122e4a1ce1f7c221a7243f9"
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhaka"
+
+// async function checkWeather(){
+//     const response = await fetch(apiUrl + `&appid=${apiKye}`)
+//     var data = await response.json()
+
+//     console.log(data)
+// }
+// checkWeather()
